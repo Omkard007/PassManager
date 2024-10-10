3353d283233a484a87164eb9a410ee1aed9609b8
SKB <shubhambhilare4899223@gmail.com>
2024-10-10 23:13:12 +0530

    responsive fixes

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let showtoast = (msg,type) => {
   }
   else toastcontainer.children[0].innerHTML=`<svg viewBox="0 0 24 24" width="100%" height="100%" fill="#07bc0c"><path d="M12 0a12 12 0 1012 12A12.014 12.014 0 0012 0zm6.927 8.2l-6.845 9.289a1.011 1.011 0 01-1.43.188l-4.888-3.908a1 1 0 111.25-1.562l4.076 3.261 6.227-8.451a1 1 0 111.61 1.183z"></path></svg>`
   toastcontainer.children[1].innerText = msg;
-  toastcontainer.style.top = "10px";
+  toastcontainer.style.top = "0px";
   setTimeout(() => {
     toastcontainer.style.top = "-1000px";
   }, 2000);
